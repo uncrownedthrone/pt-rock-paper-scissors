41cbff929060957414bb9a414068fd5d8da1f7dc
Justin Swiger <justin.swiger@me.com>
2019-10-09 19:30:32 -0400

    started rps game

--- public/main.js
+++ public/main.js
@@ -1,7 +1,8 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+	if (document.querySelector('h1.hello-world')) {
+		document.querySelector('h1.hello-world').textContent =
+			'Rock, Paper, Scissors!';
+	}
+};
 
-document.addEventListener('DOMContentLoaded', main)
+document.addEventListener('DOMContentLoaded', main);
