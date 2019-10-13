e358fa72c7b3f1e59564c86c798b45d413f9d0e5
Justin Swiger <justin.swiger@me.com>
2019-10-13 12:49:36 -0400

    added comp options and js

--- public/main.js
+++ public/main.js
@@ -1,8 +1,42 @@
+// defining querySelector shortcut variable to save time
+const qs = (element) => document.querySelector(element);
+
+// computer choice based on random number
+const compPick = Math.random();
+
+// setting parameters for computer choice
+if (compPick < 0.33) {
+	compPick = 'rock';
+} else if (compPick <= 0.67) {
+	compPick = 'paper';
+} else {
+	compPick = 'scissors';
+}
+
 const main = () => {
-	if (document.querySelector('h1.hello-world')) {
-		document.querySelector('h1.hello-world').textContent =
-			'Rock, Paper, Scissors!';
+	if (playerOneChoice === compPick) {
+		return 'Tie';
+	}
+	if (playerOneChoice === 'rock') {
+		if ((compPick = 'scissors')) {
+			return 'You Win';
+		} else {
+			return 'You Lose';
+		}
+	}
+	if (playerOneChoice === 'scissors') {
+		if ((compPick = 'paper')) {
+			return 'You Win';
+		} else {
+			return 'You Lose';
+		}
+	}
+	if (playerOneChoice === 'paper') {
+		if ((compPick = 'rock')) {
+			return 'You Win';
+		} else {
+			return 'You Lose';
+		}
 	}
 };
-
 document.addEventListener('DOMContentLoaded', main);
